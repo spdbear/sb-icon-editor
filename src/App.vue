b1e3cf75e88584df4bc05ed318ac85cc8748a6a1
spdbear <16793732+spdbear@users.noreply.github.com>
2020-02-13 04:59:54 +0900

    PNG保存と色入れ替えの追加
    - PNG保存はsave-svg-as-pngに丸投げ

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,47 @@
   <div id="app">
     <!-- <HelloWorld /> -->
     <h1>Bear Icon Generator</h1>
-    <p>
-      bgsize:
-      <input type="number" min="8" max="1920" v-model="bgsize" />
-      <input type="range" min="8" max="1920" v-model="bgsize" />
-    </p>
-    <svg viewBox="0 0 500 500" :width="bgsize" :height="bgsize" :style="bgstyle">
+    <svg
+      id="bearsvg"
+      viewBox="0 0 500 500"
+      :width="bgsize"
+      :height="bgsize"
+      :style="bgstyle"
+    >
       <!-- <line :x1="x1" y1="100" x2="200" y2="200" stroke="black" /> -->
-      <circle cx="250" cy="250" :r="r0" :fill="color" :stroke-width="stroke" />
-      <circle cx="250" cy="250" :r="rad" :stroke="color" fill="none" :stroke-width="stroke" />
-      <circle :cx="cx2" :cy="cy2" :r="r2" :stroke="color" fill="none" :stroke-width="stroke" />
-      <circle :cx="cx3" :cy="cy2" :r="r3" :stroke="color" fill="none" :stroke-width="stroke" />
+      <circle cx="250" :cy="cy" :r="r0" :fill="color" :stroke-width="stroke" />
+      <circle
+        cx="250"
+        :cy="cy"
+        :r="rad"
+        :stroke="color"
+        fill="none"
+        :stroke-width="stroke"
+      />
+      <circle
+        :cx="cx2"
+        :cy="cy2"
+        :r="r2"
+        :stroke="color"
+        fill="none"
+        :stroke-width="stroke"
+      />
+      <circle
+        :cx="cx3"
+        :cy="cy2"
+        :r="r3"
+        :stroke="color"
+        fill="none"
+        :stroke-width="stroke"
+      />
+      <circle
+        cx="250"
+        cy="250"
+        :r="250"
+        fill="none"
+        stroke="#000"
+        :stroke-width="previewCircleStroke"
+      />
     </svg>
 
     <p>
@@ -31,27 +61,47 @@
       <input type="range" min="0" max="360" v-model="theta" />
     </p>
     <p>
-      color: {{color}}
+      y_cor:
+      <input type="number" min="-250" max="250" v-model="y_cor" />
+      <input type="range" min="-250" max="250" v-model="y_cor" />
+    </p>
+    <p>
+      color: {{ color }}
       <input type="color" name="head" v-model="color" />
     </p>
     <p>
-      bgcolor: {{bgcolor}}
+      bgcolor: {{ bgcolor }}
       <input type="color" name="head" v-model="bgcolor" />
+    </p>
+    <p>
+      <button v-on:click="swapColor">Swap Color</button>
+      Preview circle: <input type="checkbox" id="checkbox" v-model="checked" />
+    </p>
+    <p>
+      Output Size:
+      <input type="number" min="8" max="1920" v-model="bgsize" />
+      <input type="range" min="8" max="1920" v-model="bgsize" />
+
+      <button v-on:click="downloadSvg">Download as PNG</button>
     </p>
   </div>
 </template>
 
 <script>
+import ssap from "save-svg-as-png";
+
 export default {
   name: "App",
   data: function() {
     return {
       bgsize: 500,
-      rad: 100,
-      stroke: 20,
+      rad: 150,
+      stroke: 36,
       theta: 45,
-      color: "#008101",
-      bgcolor: "#eeeeee"
+      y_cor: 20,
+      color: "#35485e",
+      bgcolor: "#41b883",
+      checked: false
     };
   },
   computed: {
@@ -70,11 +120,29 @@ export default {
     cx3: function() {
       return 250 + this.rad * Math.sin((this.theta * Math.PI) / 180);
     },
+    cy: function() {
+      return 250 + Number(this.y_cor);
+    },
     cy2: function() {
-      return 250 - this.rad * Math.cos((this.theta * Math.PI) / 180);
+      return this.cy - this.rad * Math.cos((this.theta * Math.PI) / 180);
     },
     bgstyle: function() {
-      return "background: " + this.bgcolor;
+      return `background: ${this.bgcolor}`;
+    },
+    previewCircleStroke: function() {
+      const stroke = this.checked ? 1 : 0;
+      return stroke;
+    }
+  },
+  methods: {
+    downloadSvg: function() {
+      const name = `${this.bgsize}x${this.bgsize}-r${this.rad}-s${this.stroke}-t${this.theta}-y${this.y_cor}-${this.color}-${this.bgcolor}`;
+      ssap.saveSvgAsPng(document.getElementById("bearsvg"), `${name}.png`);
+    },
+    swapColor: function() {
+      const tmpMainColor = this.color;
+      this.color = this.bgcolor;
+      this.bgcolor = tmpMainColor;
     }
   }
 };
